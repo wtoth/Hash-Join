ff80d62d99d5e29561c251642aaf067f862b42c7
wtoth <wtoth@umich.edu>
2020-04-12 15:11:36 -0400

    Cleaned up code and added a json object

--- join-functions.py
+++ join-functions.py
@@ -1,20 +1,13 @@
 import json
 
-import json
-
 with open('C:\\Users\\slugg\\Documents\\GitHub\\Hash-Join\\Hash-Join\\data1.json') as a:
     data1 = json.load(a)
-
 with open('C:\\Users\\slugg\\Documents\\GitHub\\Hash-Join\\Hash-Join\\data2.json') as b: 
     data2 = json.load(b)
 
-# Output: {'name': 'Bob', 'languages': ['English', 'Fench']}
-print(data1)
-print((type(data1)))
-print(data2)
+totalBuckets = 9
 
-hashTable = [[] for _ in range(10)]
-print(hashTable)
+hashTable = [[] for _ in range(totalBuckets)]
 
 def hashVal(key):
     return key % len(hashTable)
@@ -27,9 +20,6 @@ def insert(hashTable, key, value):
     hashKey = hashVal(intKey)
     hashTable[hashKey].append(value)
 
-#insert(hashTable, 5, 'Nepal')
-#insert(hashTable, 5, 'Naples')
-
 for key, value in data1.items():
     insert(hashTable, key, value)
 
@@ -37,4 +27,3 @@ for key, value in data2.items():
     insert(hashTable, key, value)
 
 print(hashTable)
-
